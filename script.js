f1df7f61647a7f02fff32b78cdf0c5b5858bd4fb
Glória Lelis <lelisgloria77@gmail.com>
2025-03-02 18:02:29 -0400

    slider, informações novas

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ function abrirEmail(email) {
     var link = "mailto:" + email + "?subject=Orçamento&body=" + encodeURIComponent(mensagem);
     window.location.href = link;
   }
+
+
+  const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+    grabCursor:true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      620: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      },
+    }
+    
+  });
